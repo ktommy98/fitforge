d512120a414069864f3d30ca4f0d0b09a769e0c1
ktommy98 <kthomasfmm@gmail.com>
2025-03-18 20:15:40 +0100

    Deploy error

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-// App.jsx
 import { useEffect, useState } from "react";
 import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
 import { Authenticator, useAuthenticator } from "@aws-amplify/ui-react";
 import Home from "./Home";
-import WorkoutPlan from "./pages/WorkoutPlan";
+import WorkoutPlan from "./pages/Workoutplan";
 import Ebook1 from "./pages/Ebook1";
 import Ebook2 from "./pages/Ebook2";
 import ProfileSetup from "./pages/ProfileSetup";
@@ -11,14 +10,12 @@ import Recipes from "./Recipes";
 
 function AppRoutes() {
   const { authStatus } = useAuthenticator((context) => [context.authStatus]);
-  // Inicializáld a profileCompleted értéket a localStorage-ból
   const [profileCompleted, setProfileCompleted] = useState(() => {
     return localStorage.getItem("profileCompleted") === "true";
   });
   const navigate = useNavigate();
 
   useEffect(() => {
-    // Csak akkor redirectelj, ha a felhasználó be van jelentkezve, és a profil NINCS kitöltve
     if (authStatus === "authenticated" && !profileCompleted) {
       navigate("/profile-setup");
     }
